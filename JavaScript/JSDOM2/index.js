c146dd22e7e609088fdeed87283f5300236f285b
ramgpal <ramgpal912@gmail.com>
2023-04-14 00:11:38 +0530

    ThirdCommit

--- JavaScript/JSDOM2/index.js
+++ JavaScript/JSDOM2/index.js
@@ -10,8 +10,30 @@ function print() {
 document.addEventListener('click',print);
 document.removeEventListener('click',print); */
 
-//Phases of event
+//EventObject
 const content=document.querySelector('#wrapper');
 content.addEventListener('click',function(event) {
 console.log(event);
 });
+
+//.preventDefault()
+let  links = document.querySelectorAll('a');
+let thirdLink = links[2];
+thirdLink.addEventListener('click', function(event)
+{
+    event.preventDefault();
+    console.log('Maja aa gya');
+});  
+
+//Avoid too many elements
+let myDiv = document.createElement('div');
+function paraStatus(event) {
+    console.log('Para '+event.target.textContent);
+}
+myDiv.addEventListener('click',paraStatus);
+for(let i=1;i<=100;i++) {
+    let newElement = document.createElement('p');
+    newElement.textContent = 'This is para' + i;
+    myDiv.appendChild(newElement);
+}
+document.body.appendChild(myDiv);
